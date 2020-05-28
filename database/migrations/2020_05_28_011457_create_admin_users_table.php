7d960d7228538e6c0848cb156f267b883bca040d
liysong <lys97693650@163.com>
2020-05-28 09:57:18 +0800

    后台管理员用户模型以及数据填充并安装JWT扩展

---
+++ database/migrations/2020_05_28_011457_create_admin_users_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAdminUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('admin_users', function (Blueprint $table) {
+            $table->id();
+            $table->string('user_name')->comment('用户名');
+            $table->string('password')->comment('密码');
+            $table->string('nickname')->comment('昵称');
+            $table->string('position')->nullable()->comment('职位');
+            $table->string('mobile')->nullable()->comment('电话');
+            $table->string('avatar')->nullable()->comment('头像');
+            $table->tinyInteger('status')->default(0)->comment('状态：0正常 1密码错误被锁定  2 超级管理员');
+            $table->dateTime('lock_time')->nullable()->comment('锁定时间');
+            $table->dateTime('update_password_time')->nullable()->comment('修改密码时间');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('admin_users');
+    }
+}
---
+++ database/seeds/AdminUserSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class AdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \App\Models\AdminUser::query()->create([
+            'user_name' => 'liysong',
+            'password' => bcrypt('liysong'),
+            'nickname' => 'maxBoss',
+            'position' => '',
+            'avatar' => 'http://image.yy.com/yywebalbumbs2bucket/144152f8680f421599233c6ffcfcef49_1476265267104.jpeg',
+            'status' => 2,
+        ]);
+    }
+}
